9775503af0cb3fe80aa74ed9475b80cd078135de
Siwarak Kongsra <gtoboo_001@hotmail.com>
2021-05-15 08:41:18 +0700

    Initial Git

---
+++ UpdateSpreadsheets/Models/DoppleAPI.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace UpdateSpreadsheets.Models
+{
+    public class DoppleAPI
+    {
+        // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+        [JsonPropertyName("dopple_price_usd")]
+        public double DopplePriceUsd { get; set; }
+
+        [JsonPropertyName("staking_apy")]
+        public StakingApyModel StakingApy { get; set; }
+
+        [JsonPropertyName("pool")]
+        public PoolModel Pool { get; set; }
+
+        [JsonPropertyName("dopple_data")]
+        public DoppleDataModel DoppleData { get; set; }
+
+        [JsonPropertyName("total_value_lock")]
+        public TotalValueLockModel TotalValueLock { get; set; }
+
+        [JsonPropertyName("latest_update")]
+        public DateTime LatestUpdate { get; set; }
+        public class StakingApyModel
+        {
+            [JsonPropertyName("cake_lp_apy")]
+            public double CakeLpApy { get; set; }
+
+            [JsonPropertyName("dop_lp_apy")]
+            public double DopLpApy { get; set; }
+
+            [JsonPropertyName("busd_lp_apy")]
+            public double BusdLpApy { get; set; }
+
+            [JsonPropertyName("dop_apy")]
+            public double DopApy { get; set; }
+
+            [JsonPropertyName("two_pool_lp_apy")]
+            public double TwoPoolLpApy { get; set; }
+
+            [JsonPropertyName("ust_lp_apy")]
+            public double UstLpApy { get; set; }
+
+            [JsonPropertyName("dolly_lp_apy")]
+            public double DollyLpApy { get; set; }
+
+            [JsonPropertyName("dolly_cake_lp_apy")]
+            public double DollyCakeLpApy { get; set; }
+        }
+
+        public class _0x5162f992EDF7101637446ecCcD5943A9dcC63A8A
+        {
+            [JsonPropertyName("apy")]
+            public double Apy { get; set; }
+
+            [JsonPropertyName("balance")]
+            public double Balance { get; set; }
+
+            [JsonPropertyName("trading_volume")]
+            public double TradingVolume { get; set; }
+        }
+
+        public class _0x830e287ac5947B1C0DA865dfB3Afd7CdF7900464
+        {
+            [JsonPropertyName("apy")]
+            public double Apy { get; set; }
+
+            [JsonPropertyName("balance")]
+            public double Balance { get; set; }
+
+            [JsonPropertyName("trading_volume")]
+            public double TradingVolume { get; set; }
+        }
+
+        public class _0x449256e20ac3ed7F9AE81c2583068f7508d15c02
+        {
+            [JsonPropertyName("apy")]
+            public double Apy { get; set; }
+
+            [JsonPropertyName("balance")]
+            public double Balance { get; set; }
+
+            [JsonPropertyName("trading_volume")]
+            public double TradingVolume { get; set; }
+        }
+
+        public class _0x61f864a7dFE66Cc818a4Fd0baabe845323D70454
+        {
+            [JsonPropertyName("apy")]
+            public double Apy { get; set; }
+
+            [JsonPropertyName("balance")]
+            public double Balance { get; set; }
+
+            [JsonPropertyName("trading_volume")]
+            public double TradingVolume { get; set; }
+        }
+
+        public class PoolModel
+        {
+            [JsonPropertyName("0x5162f992EDF7101637446ecCcD5943A9dcC63A8A")]
+            public _0x5162f992EDF7101637446ecCcD5943A9dcC63A8A _0x5162f992EDF7101637446ecCcD5943A9dcC63A8A { get; set; }
+
+            [JsonPropertyName("0x830e287ac5947B1C0DA865dfB3Afd7CdF7900464")]
+            public _0x830e287ac5947B1C0DA865dfB3Afd7CdF7900464 _0x830e287ac5947B1C0DA865dfB3Afd7CdF7900464 { get; set; }
+
+            [JsonPropertyName("0x449256e20ac3ed7F9AE81c2583068f7508d15c02")]
+            public _0x449256e20ac3ed7F9AE81c2583068f7508d15c02 _0x449256e20ac3ed7F9AE81c2583068f7508d15c02 { get; set; }
+
+            [JsonPropertyName("0x61f864a7dFE66Cc818a4Fd0baabe845323D70454")]
+            public _0x61f864a7dFE66Cc818a4Fd0baabe845323D70454 _0x61f864a7dFE66Cc818a4Fd0baabe845323D70454 { get; set; }
+        }
+
+        public class DoppleDataModel
+        {
+            [JsonPropertyName("dopple_price_usd")]
+            public double DopplePriceUsd { get; set; }
+
+            [JsonPropertyName("total_trading_volume")]
+            public double TotalTradingVolume { get; set; }
+
+            [JsonPropertyName("dopple_total_supply")]
+            public double DoppleTotalSupply { get; set; }
+
+            [JsonPropertyName("dopple_max_supply")]
+            public double DoppleMaxSupply { get; set; }
+
+            [JsonPropertyName("dopple_per_block")]
+            public double DopplePerBlock { get; set; }
+
+            [JsonPropertyName("market_cap")]
+            public double MarketCap { get; set; }
+        }
+
+        public class TvlListModel
+        {
+            [JsonPropertyName("dopple_bnb")]
+            public double DoppleBnb { get; set; }
+
+            [JsonPropertyName("dopple_busd")]
+            public double DoppleBusd { get; set; }
+
+            [JsonPropertyName("dopply_dolly")]
+            public double DopplyDolly { get; set; }
+
+            [JsonPropertyName("dop")]
+            public double Dop { get; set; }
+
+            [JsonPropertyName("stable_coin_pool")]
+            public double StableCoinPool { get; set; }
+
+            [JsonPropertyName("ust_pool")]
+            public double UstPool { get; set; }
+
+            [JsonPropertyName("two_pool")]
+            public double TwoPool { get; set; }
+
+            [JsonPropertyName("dollpy_pool")]
+            public double DollpyPool { get; set; }
+        }
+
+        public class TotalValueLockModel
+        {
+            [JsonPropertyName("tvl_list")]
+            public TvlListModel TvlList { get; set; }
+
+            [JsonPropertyName("tvl_sum")]
+            public double TvlSum { get; set; }
+        }
+    }
+}
---
+++ UpdateSpreadsheets/Program.cs
@@ -0,0 +1,123 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using UpdateSpreadsheets.Models;
+using System.Text.Json;
+
+namespace UpdateSpreadsheets
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            try
+            {
+                // Define request parameters.
+                var reqDopple = await RequestDoppleAPI();
+                String spreadsheetId = "1b5dyW7E_n-7HM-gqtYnD0lqaAsd8uqO0j7XDqMs6zBg";
+
+                var valueRange = GenerateValueRange(reqDopple.DopplePriceUsd, "API Connector!A2");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Update Price DOP (${ reqDopple.DopplePriceUsd }) to {valueRange.Item2} Successfully !");
+
+                valueRange = GenerateValueRange(reqDopple.DoppleData.MarketCap, "API Connector!AA2");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Update DOP MarketCap (${ reqDopple.DoppleData.MarketCap }) to {valueRange.Item2} Successfully !");
+
+                valueRange = GenerateValueRange(reqDopple.DoppleData.TotalTradingVolume, "API Connector!W2");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Update DOP TradingVolume (${ reqDopple.DoppleData.TotalTradingVolume }) to {valueRange.Item2} Successfully !");
+                
+                valueRange = GenerateValueRange(reqDopple.DoppleData.DoppleTotalSupply, "API Connector!X2");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Update DOP TotalSupply (${ reqDopple.DoppleData.DoppleTotalSupply }) to {valueRange.Item2} Successfully !");
+
+                valueRange = GenerateValueRange(reqDopple.TotalValueLock.TvlSum, "API Connector!AJ2");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Update DOP Total Value Lock (${ reqDopple.TotalValueLock.TvlSum }) to {valueRange.Item2} Successfully !");
+
+                var updatedDate = DateTime.Now.ToString("dd/MM/yyyy hh:mm tt");
+                valueRange = GenerateValueRange(updatedDate, "Summary!F18");
+                UpdateGoogleRequest(service, valueRange.Item1, spreadsheetId, valueRange.Item2);
+                Console.WriteLine($"Updated Date (${ updatedDate }) to {valueRange.Item2} Successfully !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Found exception Type: {ex.GetType()} then cannot Update Price");
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static UpdateValuesResponse UpdateGoogleRequest(SheetsService service, ValueRange valueRange, string spreadsheetId, string range)
+        {
+            SpreadsheetsResource.ValuesResource.UpdateRequest updateReq = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, range);
+            updateReq.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            UpdateValuesResponse result = updateReq.Execute();
+            return result;
+        }
+
+        private static Tuple<ValueRange, string> GenerateValueRange(dynamic value, string range)
+        {
+            ValueRange valueRange = new ValueRange();
+            valueRange.MajorDimension = "COLUMNS";//"ROWS";//COLUMNS
+
+            var oblist = new List<object>() { value };
+            valueRange.Values = new List<IList<object>> { oblist };
+            return Tuple.Create(valueRange, range);
+        }
+
+        private static async Task<DoppleAPI> RequestDoppleAPI()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36");
+
+            var stringTask = client.GetStringAsync("https://dopple-api.kowito.com");
+
+            var msg = await stringTask;
+            DoppleAPI myDeserializedClass = JsonSerializer.Deserialize<DoppleAPI>(msg);
+            return myDeserializedClass;
+            //Console.Write(msg);
+        }
+    }
+}
